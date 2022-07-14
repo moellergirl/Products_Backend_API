2014b0250322853fecc47a759e79d95157042313
Hot Mama <hotmama@Hots-MacBook-Pro.local>
2022-07-14 11:49:25 -0700

    Created product mondel for products app + registerd products  and  rest framework in project settings installed apps section

--- backend_api_project/settings.py
+++ backend_api_project/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&7t7g1*4rw!lps%ayazh62)botz44ufg56_gcqj7!(p=m73f8*'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'products',
 ]
 
 MIDDLEWARE = [
--- products/models.py
+++ products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class product (models.Model):
+    title=models.CharField (max_length=255)
+    description=models.CharField(max_length=500)
+    price=models.DecimalField(max_digits=4, decimal_places=2)
+    inventory_quantity=models.IntegerField()
